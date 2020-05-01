64a082062f9a8ef42414274f1fe25e7ee2616474
covid19-dashboard-us <covid19.data.method@gmail.com>
2020-05-01 12:12:17 -0400

    fourth trial

--- eg.R
+++ eg.R
@@ -3,6 +3,7 @@ rm(list = ls())
 library(BPST)
 library(mgcv)
 library(MGLM)
+library(plotly)
 
 data("county.loc")
 data("dat.fit")
@@ -19,13 +20,11 @@ tmp = "y.infec ~ Control2 + f(lI)"
 formula <- as.formula(paste0(tmp, paste0('+ s(', cov.names,', bs = \'cr\', k = 4)', collapse = '')))
 mfit.infec = fit.stem(formula, location = county.loc, V = V, Tr = Tr, d = 2, r = 1,
   family = ziP(), data = dat.fit, offset = offset)
-# save(mfit.infec, file = "mfit.infec.rda")
 
 tmp = "y.death ~ lI + Control2"
 formula <- as.formula(paste0(tmp, paste0('+ ', cov.names[-c(9,11)], collapse = '')))
 mfit.death = fit.stem(formula, location = county.loc, V = V, Tr = Tr, d = 2, r = 1,
   family = ziP(), data = dat.fit)
-# save(mfit.death, file = "mfit.death.rda")
 
 # one-day prediction
 data("dat.pred.infec")
@@ -36,5 +35,9 @@ mpred.death = predict.stem(mfit.death, dat.pred.death, adjust = FALSE)
 # seven-day rolling prediction
 date.est = as.Date("2020-04-14")
 pred.h = 7
+data("dat.pred")
 pred.rolling = predict.rolling.stem(mfit.infec, mfit.death, data.new, cov.names,
-  R.rate, pred.h, aq = c(0.001,0.999))
+  R.rate = 0.1, pred.h, aq = c(0.001,0.999))
+
+# plots
+plot.stem(mfit.infec)
